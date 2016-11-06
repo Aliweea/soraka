a1d485f04977f5b4b0d10bc96ea80c42b2a47e4e
qyc <originchenqy@gmail.com>
2016-11-06 23:27:04 +0800

    增加土地路由filter

--- src/app/filter/Environment/EnvironmentDetailFilter.js
+++ src/app/filter/Environment/EnvironmentDetailFilter.js
@@ -15,6 +15,24 @@ export default($rootScope) => {
       case 4002:
         out ="app.environmentdetail.waterCondition";
         break;
+      case 4007:
+        out="app.environmentdetail.commercialLandGross";
+        break;
+      case 4008:
+        out="app.environmentdetail.commercialLandArea";
+        break;
+      case 4012:
+        out="app.environmentdetail.industryLandGross";
+        break;
+      case 4013:
+        out="app.environmentdetail.industryLandArea";
+        break;
+      case 4009:
+        out="app.environmentdetail.illegalLandArea";
+        break;
+      case 4011:
+        out="app.environmentdetail.cultivateLandArea";
+        break;
       }
       return out;
   }
