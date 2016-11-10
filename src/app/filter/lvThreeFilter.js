27bd0f61bdc2625e91334cf46d5d68e0245ad479
winter <1944844113@qq.com>
2016-11-10 23:15:05 +0800

    bug解决

--- src/app/filter/lvThreeFilter.js
+++ src/app/filter/lvThreeFilter.js
@@ -4,6 +4,7 @@ export default($rootScope) => {
   // Needed for the loading screen
   return (input) => {
     switch (input) {
+         case '2301':
         return 'UBEI';
         break;
         case '2302':
