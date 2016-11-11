f232f56668d5db9bdc457a22406f3ad62debc46a
winter <1944844113@qq.com>
2016-11-11 14:19:52 +0800

    filter完善了下

--- src/app/filter/lvThreeFilter.js
+++ src/app/filter/lvThreeFilter.js
@@ -7,6 +7,105 @@ export default($rootScope) => {
         case '2301':
         return 'UBEI';
         break;
+        case '2302':
+        return 'UBEI';
+        break;
+        case '2303':
+        return 'UBEI';
+        break;
+        case '2304':
+        return 'UBEI';
+        break;
+        case '2311':
+        return 'UI';
+        break;
+        case '2312':
+        return 'UI';
+        break;
+        case '2313':
+        return 'UI';
+        break;
+        case '2315':
+        return 'UI';
+        break;
+        case '2321':
+        return 'UBMI';
+        break;
+        case '2322':
+        return 'UBMI';
+        break;
+        case '2323':
+        return 'UBMI';
+        break;
+        case '2324':
+        return 'UBMI';
+        break;
+          case '2331':
+        return 'URMI';
+        break;
+        case '2332':
+        return 'URMI';
+        break;
+        case '2333':
+        return 'URMI';
+        break;
+        case '2334':
+        return 'URMI';
+        break;
+        case '2341':
+        return 'EII';
+        break;
+        case '2342':
+        return 'EII';
+        break;
+        case '2343':
+        return 'EII';
+        break;
+        case '2344':
+        return 'EII';
+        break;
+        case '2345':
+        return 'EII';
+        break;
+        case '2322':
+        return 'UBI';
+        break;
+        case '2323':
+        return 'UBI';
+        break;
+        case '2324':
+        return 'UBI';
+        break;
+         case '2351':
+        return 'MI';
+        break;
+        case '2352':
+        return 'MI';
+        break;
+        case '2353':
+        return 'MI';
+        break;
+        case '2354':
+        return 'MI';
+        break;
+        case '2355':
+        return 'MI';
+        break;
+        case '2361':
+        return 'RBEI';
+        break;
+        case '2362':
+        return 'RBEI';
+        break;
+        case '2363':
+        return 'RBEI';
+        break;
+        case '2364':
+        return 'RBEI';
+        break;
+        case '2365':
+        return 'RBEI';
+        break;
         case '2231':
         return 'PopulationStructure';
         break;     
