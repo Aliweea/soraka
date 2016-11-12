605b3694757459ae116f0aa7e85fd116bab66659
Sam <1541491@tongji.edu.cn>
2016-11-13 03:20:04 +0800

    punish完成

--- src/app/in/citymanager/three/punish/cmpunishCtrl.js
+++ src/app/in/citymanager/three/punish/cmpunishCtrl.js
@@ -1,7 +1,7 @@
-export default($scope, $state, qService, dataDetailFactory, dateService) => {
+export default($scope, $rootScope, $state, qService, dataDetailFactory, dateService) => {
 	'ngInject';
 
-	const chartStore = (type, title, yTitle, xTitle, xData, yData) => {
+	const chartStore = (type, title, yTitle, xTitle, simpleData, normalData, xData) => {
 		return {
 			options: {
 				chart: {
@@ -60,9 +60,13 @@ export default($scope, $state, qService, dataDetailFactory, dateService) => {
                 borderWidth: 0
 			},
 			series:[{
-				color: '#2F4172',
-				name: yTitle,
-				data: yData
+				color: 'rgb(124, 181, 236)',
+				name: "简易程序",
+				data: simpleData
+			}, {
+				color: 'rgb(31, 194, 43)',
+				name: "一般程序",
+				data: normalData
 			}]
 		};
 	}
@@ -89,12 +93,15 @@ export default($scope, $state, qService, dataDetailFactory, dateService) => {
 				tooltip: {
 					pointFormat: tooltip
 				},
+				legend: {
+					lineHeight: 10,
+				},
 				plotOptions: {
 					pie: {
 						allowPointSelect: true,
 						cursor: 'pointer',
 						dataLabels: {
-							enabled: true,
+							enabled: false,
 							color: '#000000',
 							connectorColor: '#000000',
 							format: dataFormat
@@ -114,11 +121,13 @@ export default($scope, $state, qService, dataDetailFactory, dateService) => {
 		$(".navbar2position").hide(0); // 显示当前位置
 		$(".navbar2return").show(0); // 显示返回按钮
 		$(".navTopShowMark").hide(0); // 隐藏KPI状态 KPI分类
-		$('#cmrefuse-s1').focus();
-		$('#cmRefuseTownTooglePanel').hide(0);
-		$('#cmRefuseTownToogleButton').click(() => {
-			$('#cmRefuseTownTooglePanel').toggle(0);
-		})
+		$('#cmPunishTownTooglePanel').hide(0);
+		$('#cmPunishTownToogleButton').click(() => {
+			$('#cmPunishTownTooglePanel').toggle(0);
+		});
+		$('#cmPunishTownTooglePanel').click(() => {
+			$('#cmPunishTownTooglePanel').hide(300);
+		});
 	}();
 
 	let token = "eyJhY2NvdW50Ijp7IkBpZCI6IjEiLCJpZCI6MjYsImNyZWF0ZV90aW1lIjoiMjAxNS0wNS0xNCAxOTo1MToxNSIsIm1vZGlmeV90aW1lIjoiMjAxNS0wNy0wNSAxNDozNDozMyIsImFjY291bnQiOiJzeXN0ZW0iLCJwYXNzd29yZCI6bnVsbCwidGl0bGUiOiLnrqHnkIblkZgiLCJuYW1lIjoi566h55CG5ZGYIiwic3lzdGVtTmFtZSI6bnVsbCwibW9iaWxlUGhvbmUiOm51bGwsIndvcmtQaG9uZSI6bnVsbCwicm9sZSI6IkFETUlOSVNUUkFUT1IiLCJkZXBhcnRtZW50cyI6W3siQGlkIjoiMiIsImlkIjoxMSwiY3JlYXRlX3RpbWUiOiIyMDE0LTEyLTI5IDEyOjM3OjM5IiwibW9kaWZ5X3RpbWUiOiIyMDE0LTEyLTI5IDEyOjM3OjM5IiwibmFtZSI6IuiuoeeUn+WnlCIsImRlc2NyaXB0aW9uIjoiSlNXX+iuoeeUn+WnlCJ9LHsiQGlkIjoiMyIsImlkIjozMywiY3JlYXRlX3RpbWUiOiIyMDE1LTA0LTIwIDEzOjU2OjAyIiwibW9kaWZ5X3RpbWUiOiIyMDE1LTA0LTIwIDEzOjU2OjAyIiwibmFtZSI6IuWuieebkeWxgCIsImRlc2NyaXB0aW9uIjoiQUpKX+WuieebkeWxgCJ9LHsiQGlkIjoiNCIsImlkIjo2LCJjcmVhdGVfdGltZSI6IjIwMTQtMTItMjkgMTI6NDE6NTciLCJtb2RpZnlfdGltZSI6IjIwMTQtMTItMjkgMTI6NDE6NTciLCJuYW1lIjoi5biC5Lqk6K2m5aSn6ZifIiwiZGVzY3JpcHRpb24iOiJTSkpERF/luILkuqTorablpKfpmJ8ifSx7IkBpZCI6IjUiLCJpZCI6MjYsImNyZWF0ZV90aW1lIjoiMjAxNC0xMi0yOSAxMjozODozNCIsIm1vZGlmeV90aW1lIjoiMjAxNC0xMi0yOSAxMjozODozNCIsIm5hbWUiOiLkurrnpL7lsYAiLCJkZXNjcmlwdGlvbiI6IlJTSl/kurrnpL7lsYAifSx7IkBpZCI6IjYiLCJpZCI6MzIsImNyZWF0ZV90aW1lIjoiMjAxNS0wNC0yMCAxMzo1NjoyMCIsIm1vZGlmeV90aW1lIjoiMjAxNS0wNC0yMCAxMzo1NjowMiIsIm5hbWUiOiLkv6Horr/lsYAiLCJkZXNjcmlwdGlvbiI6IlhGSl/kv6Horr/lsYAifSx7IkBpZCI6IjciLCJpZCI6MiwiY3JlYXRlX3RpbWUiOiIyMDE1LTAxLTMwIDE3OjUyOjM0IiwibW9kaWZ5X3RpbWUiOiIyMDE1LTAxLTMwIDE3OjUyOjM0IiwibmFtZSI6Iui0ouaUv+WxgCIsImRlc2NyaXB0aW9uIjoiQ1pKX+i0ouaUv+WxgCJ9LHsiQGlkIjoiOCIsImlkIjozMCwiY3JlYXRlX3RpbWUiOiIyMDE0LTEyLTI5IDEyOjQ1OjUxIiwibW9kaWZ5X3RpbWUiOiIyMDE0LTEyLTI5IDEyOjQ1OjUxIiwibmFtZSI6IuS6pOmAmuWxgCIsImRlc2NyaXB0aW9uIjoiSlRKX+S6pOmAmuWxgCJ9LHsiQGlkIjoiOSIsImlkIjo1LCJjcmVhdGVfdGltZSI6IjIwMTUtMDEtMDMgMjI6MzM6MzUiLCJtb2RpZnlfdGltZSI6IjIwMTUtMDEtMDMgMjI6MzM6MzUiLCJuYW1lIjoi5raI6Ziy5bGAIiwiZGVzY3JpcHRpb24iOiJYRkpf5raI6Ziy5bGAIn0seyJAaWQiOiIxMCIsImlkIjoyOSwiY3JlYXRlX3RpbWUiOiIyMDE0LTEyLTI5IDEyOjQ0OjAyIiwibW9kaWZ5X3RpbWUiOiIyMDE0LTEyLTI5IDEyOjQ0OjAyIiwibmFtZSI6IuaVmeiCsuWxgCIsImRlc2NyaXB0aW9uIjoiSllKX+aVmeiCsuWxgCJ9LHsiQGlkIjoiMTEiLCJpZCI6OSwiY3JlYXRlX3RpbWUiOiIyMDE0LTEyLTI5IDEyOjM5OjI4IiwibW9kaWZ5X3RpbWUiOiIyMDE0LTEyLTI5IDEyOjM5OjI4IiwibmFtZSI6IueJqeS7t+WxgCIsImRlc2NyaXB0aW9uIjoiV0pKX+eJqeS7t+WxgCJ9LHsiQGlkIjoiMTIiLCJpZCI6MTYsImNyZWF0ZV90aW1lIjoiMjAxNC0xMC0xNiAyMTo0MTozNCIsIm1vZGlmeV90aW1lIjoiMjAxNS0wMi0wOCAxNDo1ODo1NiIsIm5hbWUiOiLpgq7mlL/lsYAiLCJkZXNjcmlwdGlvbiI6IllaSl/pgq7mlL/lsYAifSx7IkBpZCI6IjEzIiwiaWQiOjMxLCJjcmVhdGVfdGltZSI6IjIwMTUtMDQtMjAgMTM6NTY6MjMiLCJtb2RpZnlfdGltZSI6IjIwMTUtMDQtMjAgMTM6NTY6MjMiLCJuYW1lIjoi5Y2r55Sf5bGAIiwiZGVzY3JpcHRpb24iOiJXU0pf5Y2r55Sf5bGAIn0seyJAaWQiOiIxNCIsImlkIjozNCwiY3JlYXRlX3RpbWUiOiIyMDE1LTA0LTIwIDEzOjU2OjAyIiwibW9kaWZ5X3RpbWUiOiIyMDE1LTA0LTIwIDEzOjU2OjAyIiwibmFtZSI6IuawlOixoeWxgCIsImRlc2NyaXB0aW9uIjoiUVhKX+awlOixoeWxgCJ9LHsiQGlkIjoiMTUiLCJpZCI6NywiY3JlYXRlX3RpbWUiOiIyMDE0LTEyLTI5IDEyOjM3OjA1IiwibW9kaWZ5X3RpbWUiOiIyMDE0LTEyLTI5IDEyOjM3OjA1IiwibmFtZSI6Iue7n+iuoeWxgCIsImRlc2NyaXB0aW9uIjoiVEpKX+e7n+iuoeWxgCJ9LHsiQGlkIjoiMTYiLCJpZCI6MjcsImNyZWF0ZV90aW1lIjoiMjAxNC0xMi0yOSAxMjo0MTowNyIsIm1vZGlmeV90aW1lIjoiMjAxNC0xMi0yOSAxMjo0MTowNyIsIm5hbWUiOiLmtojpmLLlpKfpmJ8iLCJkZXNjcmlwdGlvbiI6IlhGRERf5raI6Ziy5aSn6ZifIn0seyJAaWQiOiIxNyIsImlkIjoxMiwiY3JlYXRlX3RpbWUiOiIyMDE1LTA0LTE5IDE2OjIxOjA3IiwibW9kaWZ5X3RpbWUiOiIyMDE1LTA0LTE5IDE2OjIxOjA3IiwibmFtZSI6IueOr+S/neWxgCIsImRlc2NyaXB0aW9uIjoiSEJKX+eOr+S/neWxgCJ9LHsiQGlkIjoiMTgiLCJpZCI6MTUsImNyZWF0ZV90aW1lIjoiMjAxNC0xMi0yOSAxMjozNjozMCIsIm1vZGlmeV90aW1lIjoiMjAxNC0xMi0yOSAxMjozNjozMCIsIm5hbWUiOiLlm73lnJ/lsYAiLCJkZXNjcmlwdGlvbiI6IkdUSl/lm73lnJ/lsYAifSx7IkBpZCI6IjE5IiwiaWQiOjI1LCJjcmVhdGVfdGltZSI6IjIwMTQtMTItMzEgMDk6MTI6NDgiLCJtb2RpZnlfdGltZSI6IjIwMTQtMTItMzEgMDk6MTI6NDgiLCJuYW1lIjoi57uP5rWO55u45YWz57uEIiwiZGVzY3JpcHRpb24iOiJKSlhHWl/nu4/mtY7nm7jlhbPnu4QifSx7IkBpZCI6IjIwIiwiaWQiOjE3LCJjcmVhdGVfdGltZSI6IjIwMTQtMTItMjkgMTI6NDU6MTQiLCJtb2RpZnlfdGltZSI6IjIwMTQtMTItMjkgMTI6NDU6MTQiLCJuYW1lIjoi57uP5L+h5aeUIiwiZGVzY3JpcHRpb24iOiJKWFdf57uP5L+h5aeUIn0seyJAaWQiOiIyMSIsImlkIjoyMiwiY3JlYXRlX3RpbWUiOiIyMDE0LTEyLTI5IDEyOjQwOjEwIiwibW9kaWZ5X3RpbWUiOiIyMDE0LTEyLTI5IDEyOjQwOjEwIiwibmFtZSI6IuWFrOWuieWxgCIsImRlc2NyaXB0aW9uIjoiR0FKX+WFrOWuieWxgCJ9LHsiQGlkIjoiMjIiLCJpZCI6MTMsImNyZWF0ZV90aW1lIjoiMjAxNC0xMi0yOSAxMjozNTo1NCIsIm1vZGlmeV90aW1lIjoiMjAxNC0xMi0yOSAxMjozNTo1NCIsIm5hbWUiOiLmsLTliKnlsYAiLCJkZXNjcmlwdGlvbiI6IlNMSl/msLTliKnlsYAifSx7IkBpZCI6IjIzIiwiaWQiOjI4LCJjcmVhdGVfdGltZSI6IjIwMTQtMTItMjkgMTI6NDM6MDgiLCJtb2RpZnlfdGltZSI6IjIwMTQtMTItMjkgMTI6NDM6MDgiLCJuYW1lIjoi5Z+O566h5bGAIiwiZGVzY3JpcHRpb24iOiJDR0pf5Z+O566h5bGAIn1dfSwiZXhwaXJlcyI6MTQ3OTgyMTc0MTY0OSwiZ3JhbnRlZEF1dGhzIjpbIlJPTEVfQURNSU5JU1RSQVRPUiJdLCJhY2NvdW50Tm9uTG9ja2VkIjp0cnVlLCJhY2NvdW50Tm9uRXhwaXJlZCI6dHJ1ZSwiY3JlZGVudGlhbHNOb25FeHBpcmVkIjp0cnVlLCJlbmFibGVkIjp0cnVlLCJ1c2VybmFtZSI6InN5c3RlbSIsInBhc3N3b3JkIjpudWxsfQ==.JUDLwxyDXSAtPolQ5aznMGkehbEiEx+v3mN+xk0ol2I=";
@@ -138,21 +147,23 @@ export default($scope, $state, qService, dataDetailFactory, dateService) => {
 			let sysTime = dateService.getSystemTime(); // 获取到系统设置的时间
 			let lastYear = data.data.year, 
 				lastMonth = data.data.month;
-			let lastTime = new Date(lastYear+"-"+lastMonth+"-"+"01");
+			let lastTime = new Date(lastYear+"-"+(lastMonth + 1)+"-"+"01");
 			if (sysTime < lastTime) {
 				lastTime = sysTime; // 取到最后有效时间
 			}
+			lastYear = moment(lastTime).year();
+			lastMonth = moment(lastTime).month();
 			let params = {
 				tableName: "AyhzData"
 			}
 			let body = {
 				"year": {
-			        "value1": moment(lastTime).year(),
+			        "value1": lastYear,
 			        "queryType": "eq",
 			        "valueType": "innt"
 		        },
 		        "month":{
-			        "value1":7,
+			        "value1":lastMonth,
 			        "queryType":"eq",
 			        "valueType":"innt"
 		        }
@@ -181,106 +192,31 @@ export default($scope, $state, qService, dataDetailFactory, dateService) => {
 						timesByReasonList[i] = [reasons[i], timesByReason[reasons[i]]];
 						moneyByReasonList[i] = [reasons[i], moneyByReason[reasons[i]]];
 					}
-					$scope.changeTo = (type) => {
+					$scope.change2To = (type) => {
 						switch(type) {
 							case "time":
-								$scope.pieChart = pieStore(moment(lastTime).year()+"年"+moment(lastTime).month()+"月份行政处罚案由汇总(案件数量)",
+								$scope.reasonsShowData = timesByReasonList;
+								$scope.reasonsDataUnit = "次";
+								$('#cmrefuse-s1').addClass('activeTab');
+								$('#cmrefuse-s2').removeClass('activeTab');
+								$scope.pieChart = pieStore(lastYear+"年"+lastMonth+"月份行政处罚案由汇总(案件数量)",
 									'<b>案件数量</b>:{point.y:1.f}(起)</b>',
 									timesByReasonList,
 									'{point.percentage:.1f} %');
 							break;
 							case "money":
-								$scope.pieChart = pieStore(moment(lastTime).year()+"年"+moment(lastTime).month()+"月份行政处罚案由汇总(处罚金额)",
+								$scope.reasonsShowData = moneyByReasonList;
+								$scope.reasonsDataUnit = "元";
+								$('#cmrefuse-s2').addClass('activeTab');
+								$('#cmrefuse-s1').removeClass('activeTab');
+								$scope.pieChart = pieStore(lastYear+"年"+lastMonth+"月份行政处罚案由汇总(处罚金额)",
 									'<b>处罚金额</b>:{point.y:1.f}(元)</b>',
 									moneyByReasonList,
 									'{point.percentage:.1f} %');
 							break;
 						}
-					}
-		// 			let towns = [], times = [], tons = [],
-		// 				timeStatus, tonStatus;
-		// 			for (var i = 0; i < data.data.length; i++) {
-		// 				if (data.data[i].lajiqingyundian !== 7012) { // 不显示全市数据
-		// 					towns.push(townMap[data.data[i].lajiqingyundian]);
-		// 					times.push(data.data[i].cheshu);
-		// 					tons.push(data.data[i].dunshu);
-		// 				} else {
-		// 					timeStatus = data.data[i].cheshu < 170? lastTime+"垃圾清运车次正常,"+data.data[i].cheshu+"车次(实绩)<170车次(警戒值)": lastTime+"垃圾清运车次不正常,"+data.data[i].cheshu+"车次(实绩)>170车次(警戒值)";
-		// 					tonStatus = data.data[i].dunshu < 900? lastTime+"垃圾清运吨数正常,"+data.data[i].dunshu+"吨(实绩)<900吨(警戒值)": "正常,"+data.data[i].dunshu+"吨(实绩)>900吨(警戒值)";
-		// 				}
-		// 			}
-		// 			$scope.changeChart = (type) => {
-		// 				switch (type) {
-		// 					case "time":
-		// 						$scope.oChart = chartStore("column", lastTime+"垃圾清运车次", "车次", "垃圾清运点", towns, times);
-		// 						$scope.status = timeStatus;
-		// 						$('#cmrefuse-s1').addClass('activeTab');
-		// 						$('#cmrefuse-s2').removeClass('activeTab');
-		// 						currentType = "time";
-		// 						break;
-		// 					case "ton":
-		// 						$scope.oChart = chartStore("column", lastTime+"垃圾清运吨数", "吨数", "垃圾清运点", towns, tons);
-		// 						$scope.status = tonStatus;
-		// 						$('#cmrefuse-s2').addClass('activeTab');
-		// 						$('#cmrefuse-s1').removeClass('activeTab');
-		// 						currentType = "ton";
-		// 						break;
-		// 				}
-		// 			};
-		// 			$scope.changeChart(currentType);
-		// 		} else {}
-		// 	}, (err) => {
-		// 		if (err.errorCode == "UNAUTHORIZED") {
-		// 			$state.go('portal');
-		// 		} else {}
-		// 	});
-		// 	// 最近七天数据
-		// 	params = {
-		// 		tableName: "CityManagerLajiData",
-		// 		start: startTime,
-		// 		end: lastTime
-		// 	}
-		// 	qService.httpGetbyJSOG(dataDetailFactory.query, params, headers).then((data) => {
-		// 		if (data.errorCode == "NO_ERROR") {
-		// 			let timeDataMap = {
-		// 				7001: [],7002: [],7003: [],7004: [],7005: [],7006: [],7007: [],	7008: [],7009: [],
-		// 				7010: [],7011: [],7012: [], 7013: [],7014: [],7015: [],7016: [],7017: [],7018: [],7019: []
-		// 			}, tonDataMap = {
-		// 				7001: [],7002: [],7003: [],7004: [],7005: [],7006: [],7007: [],	7008: [],7009: [],
-		// 				7010: [],7011: [],7012: [], 7013: [],7014: [],7015: [],7016: [],7017: [],7018: [],7019: []
-		// 			};
-		// 			for (var i = 0; i < data.data.length; i++) {
-		// 				timeDataMap[data.data[i].lajiqingyundian].push(data.data[i].cheshu);
-		// 				tonDataMap[data.data[i].lajiqingyundian].push(data.data[i].dunshu);
-		// 			}
-		// 			let timeLines = [
-		// 				moment(lastTime).subtract(6, 'days').date(),
-		// 				moment(lastTime).subtract(5, 'days').date(),
-		// 				moment(lastTime).subtract(4, 'days').date(),
-		// 				moment(lastTime).subtract(3, 'days').date(),
-		// 				moment(lastTime).subtract(2, 'days').date(),
-		// 				moment(lastTime).subtract(1, 'days').date(),
-		// 				moment(lastTime).subtract(0, 'days').date(),
-		// 			]
-		// 			$scope.changeTown = (name) => {
-		// 				$scope.currentTownName = name; // 记录当前显示的城镇name
-		// 				$('#cmRefuseTownTooglePanel').hide(0);
-		// 				let id = 7012;
-		// 				for(var key in townMap) {
-		// 					if (townMap[key] === name) {
-		// 						id = key;
-		// 					}
-		// 				}
-		// 				switch (currentType) {
-		// 					case "time":
-		// 						$scope.townChart = chartStore("spline", "近七天"+townMap[id]+"地区垃圾清运车数", "车数", "日期", timeLines, timeDataMap[id]);
-		// 						break;
-		// 					case "ton":
-		// 						$scope.townChart = chartStore("spline", "近七天"+townMap[id]+"地区垃圾清运吨数", "吨数", "日期", timeLines, tonDataMap[id]);		
-		// 						break;
-		// 				}
-		// 			}
-		// 			$scope.changeTown($scope.currentTownName);
+					};
+					$scope.change2To("time");
 				} else {}
 			}, (err) => {
 				if (err.errorCode == "UNAUTHORIZED") {
@@ -288,7 +224,79 @@ export default($scope, $state, qService, dataDetailFactory, dateService) => {
 				} else {}
 			}).finally(() => {
 		        $rootScope.loading = false;
-		    });	
+		    });
+			// 获取各执法中队处理案件详情
+			params = {
+				tableName: "AdministrativePenaltyData"
+			}
+		    body = {
+				"year": {
+			        "value1": lastYear,
+			        "queryType": "eq",
+			        "valueType": "innt"
+		        }
+			}
+			$rootScope.loading = true;
+			qService.httpPost(dataDetailFactory.advancedQuery, params, headers, body).then((data) => {
+				if (data.errorCode == "NO_ERROR") {
+					let lochus = [], // 所有中队
+						simpletimes = [], simpletimesSum = 0,
+						simplemoney = [], simplemoneySum = 0,
+						normaltimes = [], normaltimesSum = 0,
+						normalmoney = [], normalmoneySum = 0;
+					for (var i = data.data.length - 1; i >= 0; i--) {
+						if (lochus.indexOf(data.data[i].zdname) === -1 && data.data[i].month === lastMonth) {
+							lochus.push(data.data[i].zdname);
+							simpletimes.push(data.data[i].jy_BYAFS); simpletimesSum += data.data[i].jy_BYAFS;
+							simplemoney.push(data.data[i].jy_BYJE); simplemoneySum += data.data[i].jy_BYJE;
+							normaltimes.push(data.data[i].yb_BYAFS); normaltimesSum += data.data[i].yb_BYAFS;
+							normalmoney.push(data.data[i].yb_BYJE); normalmoneySum += data.data[i].yb_BYJE;
+						};
+					}
+					$scope.oneword1 = lastYear+"年"+lastMonth+"月份全市城市管理行政处罚案件数 " + (simpletimesSum+normaltimesSum) + " 起 , 处罚金额共 "+ (simplemoneySum+normalmoneySum) +" 元。"
+					$scope.change3To = (type) => {
+						switch(type) {
+							case "time":
+								// (type, title, yTitle, xTitle, simpleData, normalData, xData)
+								$scope.lochusChart = chartStore("bar", lastYear+"年"+lastMonth+"月份各中队行政处罚案件数",
+									'案件数量(起)',
+									'中队',
+									simpletimes,
+									normaltimes,
+									lochus);
+								$scope.totalChart = chartStore("column", lastYear+"年"+lastMonth+"月份城管执法行政处罚案件数",
+									'案件数量(起)',
+									'中队',
+									[simpletimesSum],
+									[normaltimesSum],
+									["总计"]);
+							break;
+							case "money":
+								$scope.lochusChart = chartStore("bar", lastYear+"年"+lastMonth+"月份各中队行政处罚金额",
+									'处罚金额(元)',
+									'中队',
+									simplemoney,
+									normalmoney,
+									lochus);
+								$scope.totalChart = chartStore("column", lastYear+"年"+lastMonth+"月份城管执法行政处罚金额",
+									'处罚金额(元)',
+									'中队',
+									[simplemoneySum],
+									[normalmoneySum],
+									["总计"]);
+							break;
+						};
+						$scope.change2To(type);
+					};
+					$scope.change3To("time");
+				} else {}
+			}, (err) => {
+				if (err.errorCode == "UNAUTHORIZED") {
+					$state.go('portal');
+				} else {}
+			}).finally(() => {
+		        $rootScope.loading = false;
+		    });
 		} else {}
 	}, (err) => {
 		if (err.errorCode == "UNAUTHORIZED") {
