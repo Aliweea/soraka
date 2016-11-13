a8a195e838380af1377ffb4205130a2ebd335542
Li Site <8lisite@tongji.edu.cn>
2016-11-13 15:11:49 +0800

    仪表盘修改啦啦啦

--- src/app/in/Environment/three/water/waterThreeCtrl.js
+++ src/app/in/Environment/three/water/waterThreeCtrl.js
@@ -83,112 +83,6 @@ export default ($scope, kpiDetailService, dateService) => {
 
     //水质量 highcharts options
     $scope.waterQualityOptions = {
-        // o2Option: {
-        //     options: {
-        //         chart: {
-        //             type: 'gauge',
-        //             plotBackgroundColor: null,
-        //             plotBackgroundImage: null,
-        //             plotBorderWidth: 0,
-        //             plotShadow: false,
-        //             width: $(window).width() * 0.5,
-        //             height: $(window).width() * 0.6
-        //         },
-        //         exporting: {
-        //             enabled: false
-        //         },
-        //         pane: {
-        //             startAngle: -150,
-        //             endAngle: 150,
-        //             background: [{
-        //                 backgroundColor: {
-        //                     linearGradient: {
-        //                         x1: 0,
-        //                         y1: 0,
-        //                         x2: 0,
-        //                         y2: 1
-        //                     },
-        //                     stops: [
-        //                         [0, '#FFF'],
-        //                         [1, '#333']
-        //                     ]
-        //                 },
-        //                 borderWidth: 0,
-        //                 outerRadius: '109%'
-        //             }, {
-        //                 backgroundColor: {
-        //                     linearGradient: {
-        //                         x1: 0,
-        //                         y1: 0,
-        //                         x2: 0,
-        //                         y2: 1
-        //                     },
-        //                     stops: [
-        //                         [0, '#333'],
-        //                         [1, '#FFF']
-        //                     ]
-        //                 },
-        //                 borderWidth: 1,
-        //                 outerRadius: '107%'
-        //             }, {
-        //                 // default background
-        //             }, {
-        //                 backgroundColor: '#DDD',
-        //                 borderWidth: 0,
-        //                 outerRadius: '105%',
-        //                 innerRadius: '103%'
-        //             }]
-        //         },
-        //         title: {
-        //             text: '溶解氧'
-        //         },
-        //         credits: {
-        //             enabled: false
-        //         },
-        //         // the value axis
-        //         yAxis: {
-        //             min: 0,
-        //             max: 10,
-        //             minorTickInterval: 'auto',
-        //             minorTickWidth: 1,
-        //             minorTickLength: 10,
-        //             minorTickPosition: 'inside',
-        //             minorTickColor: '#666',
-        //             tickPixelInterval: 30,
-        //             tickWidth: 2,
-        //             tickPosition: 'inside',
-        //             tickLength: 10,
-        //             tickColor: '#666',
-        //             labels: {
-        //                 step: 2,
-        //                 rotation: 'auto'
-        //             },
-        //             title: {
-        //                 text: ''
-        //             },
-        //             plotBands: [{
-        //                 from: waterQualityO2Scan.scan[0],
-        //                 to: 10,
-        //                 color: '#55BF3B' // green
-        //             }, {
-        //                 from: waterQualityO2Scan.scan[1],
-        //                 to: waterQualityO2Scan.scan[0],
-        //                 color: '#DDDF0D' // yellow
-        //             }, {
-        //                 from: 0,
-        //                 to: waterQualityO2Scan.scan[1],
-        //                 color: '#DF5353' // red
-        //             }]
-        //         },
-        //     },
-        //     series: [{
-        //         name: '溶解氧',
-        //         data: [0],
-        //         tooltip: {
-        //             valueSuffix: ' '
-        //         }
-        //     }],
-        // },
         o2Option: {
             options: {
                 chart: {
@@ -197,16 +91,23 @@ export default ($scope, kpiDetailService, dateService) => {
                     plotBackgroundImage: null,
                     plotBorderWidth: 0,
                     plotShadow: false,
+                    width: $(window).width() * 0.5,
+                    height: $(window).width() * 0.6
                 },
-                title: {
-                    text: 'Speedometer'
+                exporting: {
+                    enabled: false
                 },
                 pane: {
                     startAngle: -150,
                     endAngle: 150,
                     background: [{
                         backgroundColor: {
-                            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                            linearGradient: {
+                                x1: 0,
+                                y1: 0,
+                                x2: 0,
+                                y2: 1
+                            },
                             stops: [
                                 [0, '#FFF'],
                                 [1, '#333']
@@ -216,7 +117,12 @@ export default ($scope, kpiDetailService, dateService) => {
                         outerRadius: '109%'
                     }, {
                         backgroundColor: {
-                            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                            linearGradient: {
+                                x1: 0,
+                                y1: 0,
+                                x2: 0,
+                                y2: 1
+                            },
                             stops: [
                                 [0, '#333'],
                                 [1, '#FFF']
@@ -226,6 +132,7 @@ export default ($scope, kpiDetailService, dateService) => {
                         outerRadius: '107%'
                     }, {
                         // default background
+                        backgroundColor: '#FFF'
                     }, {
                         backgroundColor: '#DDD',
                         borderWidth: 0,
@@ -233,10 +140,16 @@ export default ($scope, kpiDetailService, dateService) => {
                         innerRadius: '103%'
                     }]
                 },
+                title: {
+                    text: '溶解氧'
+                },
+                credits: {
+                    enabled: false
+                },
                 // the value axis
                 yAxis: {
                     min: 0,
-                    max: 200,
+                    max: 10,
                     minorTickInterval: 'auto',
                     minorTickWidth: 1,
                     minorTickLength: 10,
@@ -252,28 +165,28 @@ export default ($scope, kpiDetailService, dateService) => {
                         rotation: 'auto'
                     },
                     title: {
-                        text: 'km/h'
+                        text: ''
                     },
                     plotBands: [{
-                        from: 0,
-                        to: 120,
+                        from: waterQualityO2Scan.scan[0],
+                        to: 10,
                         color: '#55BF3B' // green
                     }, {
-                        from: 120,
-                        to: 160,
+                        from: waterQualityO2Scan.scan[1],
+                        to: waterQualityO2Scan.scan[0],
                         color: '#DDDF0D' // yellow
                     }, {
-                        from: 160,
-                        to: 200,
+                        from: 0,
+                        to: waterQualityO2Scan.scan[1],
                         color: '#DF5353' // red
                     }]
                 },
             },
             series: [{
-                name: 'Speed',
-                data: [100],
+                name: '溶解氧',
+                data: [0],
                 tooltip: {
-                    valueSuffix: ' km/h'
+                    valueSuffix: ' '
                 }
             }],
         },
@@ -329,6 +242,7 @@ export default ($scope, kpiDetailService, dateService) => {
                         outerRadius: '107%'
                     }, {
                         // default background
+                        backgroundColor: '#FFF'
                     }, {
                         backgroundColor: '#DDD',
                         borderWidth: 0,
@@ -435,6 +349,7 @@ export default ($scope, kpiDetailService, dateService) => {
                         outerRadius: '107%'
                     }, {
                         // default background
+                        backgroundColor: '#FFF'
                     }, {
                         backgroundColor: '#DDD',
                         borderWidth: 0,
@@ -541,6 +456,7 @@ export default ($scope, kpiDetailService, dateService) => {
                         outerRadius: '107%'
                     }, {
                         // default background
+                        backgroundColor: '#FFF'
                     }, {
                         backgroundColor: '#DDD',
                         borderWidth: 0,
