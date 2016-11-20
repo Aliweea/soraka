40339dbf069360871c3e27a764ce41e8efeecdc9
Sam <1541491@tongji.edu.cn>
2016-11-21 01:17:11 +0800

    横竖屏自适应

--- src/app/in/home/homeCtrl.js
+++ src/app/in/home/homeCtrl.js
@@ -5,23 +5,6 @@ export default ($scope, $rootScope, qService, kpiRes, xService, toolService) => 
 	$('#home_logout').show(0);
     $('#navBottomReturn').hide(0);
 	
-	// 适应屏幕高度
-	const adjustLayout = () => {
-		let clientH = $(window).height();
-		let clientW = $(window).width();
-		if (clientW < clientH) { // 竖屏
-			let contentH = clientH - 50 * 2;
-			let rowH_common = (contentH - 20) / 3;
-			$(".list-group-item-home").css("height", rowH_common + "px");
-		} //横屏将根据图片150x150的大小自动调整
-	}();
-	// 根据屏幕调整布局
-	const adjustItems = () => {
-		let clientH = $(window).height();
-		let clientW = $(window).width();
-		let midH = clientH - 126 * 2 - 51 * 2;
-		$("#list-group-middle").height(midH);
-	}();
 	let params = {
 
 	};
@@ -51,4 +34,23 @@ export default ($scope, $rootScope, qService, kpiRes, xService, toolService) => 
     }).finally(() => {
         $rootScope.loading = false;
     });
+
+    // 适应屏幕高度
+    $rootScope.loading = true;
+	const adjustLayout = () => {
+		let clientH = $(window).height();
+		let clientW = $(window).width();
+		if (clientW < clientH) { // 竖屏
+			$(".list-group-item-side").css("height", "126px");
+
+			let midH = clientH - 126 * 2 - 51 * 2;
+			$("#list-group-middle").height(midH);
+		} else {
+			$(".list-group-item-side").css("height", "126px");
+
+			let midH = clientW - 126 * 2 - 51 * 2;
+			$("#list-group-middle").height(midH);
+		}
+	}();
+	$rootScope.loading = false;
 };
