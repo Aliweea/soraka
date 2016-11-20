7a71f180017a3ab84ddfb40cb7ef0aa0c099af5b
Sam <1541491@tongji.edu.cn>
2016-11-21 01:35:37 +0800

    更改密码空值校验

--- src/app/in/changepsw/changepswCtrl.js
+++ src/app/in/changepsw/changepswCtrl.js
@@ -9,8 +9,13 @@ export default($scope, qService, accountRes, AuthTool, $state, $rootScope, $sess
     $scope.succ = "";
     $rootScope.loading = false;
     const checkInput = () => {
-        if ($scope.npsw1 !== $scope.npsw2) {
+        if (isEmpty($scope.npsw1) && isEmpty($scope.npsw2)) {
+            $scope.err = "密码不能为空!";
+            $scope.succ = null;
+            return false;
+        } else if ($scope.npsw1 !== $scope.npsw2) {
             $scope.err = "两次密码输入不一致!";
+            $scope.succ = null;
             return false;
         }
         return true;
@@ -19,6 +24,7 @@ export default($scope, qService, accountRes, AuthTool, $state, $rootScope, $sess
         if (checkInput()) {
             qService.httpPostWithToken(accountRes.resetPsw, {}, {}, { password: $scope.npsw1 }).then((data) => {
                 if (data.errorCode == "NO_ERROR") {
+                    $scope.err = null;
                     $scope.succ = "修改成功!";
                 } else {}
             }, (err) => {
@@ -30,4 +36,8 @@ export default($scope, qService, accountRes, AuthTool, $state, $rootScope, $sess
             });
         }
     }
+
+    let isEmpty = (value) => {
+        return typeof(value) == undefined || value == null || value == "";
+    };
 }
