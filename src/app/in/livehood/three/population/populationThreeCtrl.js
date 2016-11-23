b99bd18126cea8bfb28667629b694334982a2c0c
winter <1944844113@qq.com>
2016-11-23 10:19:02 +0800

    人口结构和社会保险上面下拉框文字居中完成

--- src/app/in/livehood/three/population/populationThreeCtrl.js
+++ src/app/in/livehood/three/population/populationThreeCtrl.js
@@ -2,6 +2,8 @@ export default ($scope, $state, $location, qService, dataDetailFactory, generalS
 	'ngInject';
 
 	const jQueryDOMToDos = () => {
+		$(".insurance-return").show(0); // 显示上面的返回按钮
+        $(".homepage").hide(0); // 隐藏主页键
 		$(".navbar2position").hide(0); // 隐藏下面当前位置
 		$(".navbar2return").show(0); // 显示返回按钮
 		$(".navTopShowMark").hide(0); // 隐藏KPI状态 KPI分类
