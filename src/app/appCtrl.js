48d7cc27731cccbae83b48750c780fb8bc8b82f3
Sam <1541491@tongji.edu.cn>
2016-12-06 00:50:36 +0800

    取消APP内下拉刷新——01

--- src/app/appCtrl.js
+++ src/app/appCtrl.js
@@ -2,4 +2,19 @@ export default($rootScope) => {
 	'ngInject';
 	// Needed for the loading screen
 	$rootScope.loading = false;
+
+	// APP内取消下拉刷新
+	try {
+		if (isFunction(window.webwindow.onPullDownToRefresh)) {
+			window.webwindow.onPullDownToRefresh(() => {
+				return false;
+			});
+		}
+	} catch(e) {
+		console.log('Error: ' + e);
+	}
+
+	const isFunction = (fn) => {
+	   return Object.prototype.toString.call(fn) === '[object Function]';
+	}
 };
