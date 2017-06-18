7938a38200630a65b0816d0030e464cb017ad245
qyc <originchenqy@gmail.com>
2017-06-18 11:35:36 +0800

    该分支为已部署的分支，所有要部署的先合并到该分支，然后再部署

--- src/app/filter/applyDateFilter.js
+++ src/app/filter/applyDateFilter.js
@@ -1,8 +1,9 @@
 export default() => {
   'ngInject';
   
-  const get_date = (timestamp, type) => {
+  const get_date = (timestamp, type, name) => {
     var m_names = new Array("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
+    var q_names = new Array("第1", "第2", "第3", "第4");
     var d = new Date(timestamp);
     var curr_date = d.getDate();
       var curr_month = d.getMonth();
@@ -10,7 +11,11 @@ export default() => {
     if(type=='YEARLY'){
       return curr_year + "年";
     }else if(type=='MONTHLY'){
-      return curr_year + "年" + m_names[curr_month] + "月份";
+      if (name.indexOf('GDP') > 0) {
+        return curr_year + "年" + q_names[parseInt( curr_month / 3 )] + "季度";
+      } else {
+        return curr_year + "年" + m_names[curr_month] + "月份";
+      }
     }else if (type=='DAYLY'){
       return curr_year + "年" + m_names[curr_month] + "月" + curr_date + "日" ;
     }else{
@@ -19,9 +24,10 @@ export default() => {
   }
   return (kpi) => {
     var type = kpi.type;
+    var name = kpi.name;
     var applyDate = kpi.data.applyDate;
     if(applyDate!=undefined){
-      return get_date(applyDate, type);
+      return get_date(applyDate, type, name);
     }else{
       return '无数据';
     }
